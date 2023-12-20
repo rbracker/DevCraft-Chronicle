83255ac40848beb11afc6ae2d20732dcf7ac0d22
Ranee Bracker <rbracker@asu.edu>
2023-12-20 14:19:57 -0700

    got my npm run seed working

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: 'mysql',
+  dialectOptions: {
+    decimalNumbers: true,
+  },
+  logging: false,
+});
+
+module.exports = sequelize;
